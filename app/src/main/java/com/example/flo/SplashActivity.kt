b4c3fdac0b419644de384b148fd76580572d0fd4
shjung53 <gcjung777@gmail.com>
2021-10-18 12:39:07 +0900

    splash activity

---
+++ app/src/main/java/com/example/flo/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.flo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.flo.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent (this,MainActivity::class.java)
+            startActivity(intent)
+        }, 2000)
+
+
+
+    }
+}
